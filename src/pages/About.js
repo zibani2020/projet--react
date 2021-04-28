13dcaf923c1413dbd61514883c22d0d19d894116
Zibani Meriem <zibani-meriem@outlook.fr>
2021-04-28 14:56:10 +0200

    init

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = ()  => {
+  return (
+    <div >
+        <Navigation />
+        <Logo />
+
+      <h1> A propos </h1>
+      <br/>
+      <p>jkkn</p>
+      <br/>
+      <p>jkknhgfugyugfdrytf</p>
+
+    </div>
+  );
+}
+
+export default About;
